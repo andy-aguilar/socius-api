10d2581ab410e0102f536539298d4a3b557ef58c
andy-aguilar <aaguil3@gmail.com>
2020-08-26 19:42:28 -0400

    done with JWT auth

---
+++ db/schema.rb
@@ -0,0 +1,75 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_08_26_212525) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "club_runs", force: :cascade do |t|
+    t.bigint "club_id", null: false
+    t.bigint "run_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["club_id"], name: "index_club_runs_on_club_id"
+    t.index ["run_id"], name: "index_club_runs_on_run_id"
+  end
+
+  create_table "clubs", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "runs", force: :cascade do |t|
+    t.integer "distance"
+    t.string "name"
+    t.boolean "complete"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_clubs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "club_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["club_id"], name: "index_user_clubs_on_club_id"
+    t.index ["user_id"], name: "index_user_clubs_on_user_id"
+  end
+
+  create_table "user_runs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "run_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["run_id"], name: "index_user_runs_on_run_id"
+    t.index ["user_id"], name: "index_user_runs_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email_address"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "club_runs", "clubs"
+  add_foreign_key "club_runs", "runs"
+  add_foreign_key "user_clubs", "clubs"
+  add_foreign_key "user_clubs", "users"
+  add_foreign_key "user_runs", "runs"
+  add_foreign_key "user_runs", "users"
+end
