621515469d412c30ca0d7dbe9ef8a785fefef586
andy-aguilar <aaguil3@gmail.com>
2020-09-01 05:34:55 -0400

    removed byebugs from userruns controller

--- app/controllers/user_runs_controller.rb
+++ app/controllers/user_runs_controller.rb
@@ -4,10 +4,8 @@ class UserRunsController < ApplicationController
         user = User.find(user_run_params[:user_id])
         run = Run.find(user_run_params[:run_id])
         if run.users.include?(user)
-            byebug
             render json: { error: "You've already joined #{run.name}!"}
         else
-            byebug
             user_run = UserRun.create(user_run_params)
             render json: run, include: [:users]
         end
