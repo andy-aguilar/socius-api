2e3feee07674eea40a599191651be243fe863f39
andy-aguilar <aaguil3@gmail.com>
2020-09-09 10:04:32 -0400

    reseeded to add friends

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,25 @@ val = User.create({first_name: 'Valerie', last_name: 'Aguilar', email_address: '
 ali = User.create({first_name: 'Alexia', last_name: 'Aguilar', email_address: 'ali@ali.com', password: 'andyiscool'})
 mom = User.create({first_name: 'Kattya', last_name: 'Aguilar', email_address: 'mom@mom.com', password: 'andyiscool'})
 dad = User.create({first_name: 'Humberto', last_name: 'Aguilar', email_address: 'dad@dad.com', password: 'andyiscool'})
+esther = User.create({first_name: 'Esther', last_name: 'Kang', email_address: 'esther@esther.com', password: 'esther'})
+
+dave = User.create({first_name: 'Dave', last_name: 'Frame', email_address: 'dave@dave.com', password: 'dave'})
+diego = User.create({first_name: 'Diego', last_name: 'Zegarra', email_address: 'diego@diego.com', password: 'diego'})
+zara = User.create({first_name: 'Zara', last_name: 'Ahmadi', email_address: 'zara@zara.com', password: 'zara'})
+david = User.create({first_name: 'David', last_name: 'Chung', email_address: 'david@david.com', password: 'david'})
+issa = User.create({first_name: 'Issa', last_name: 'Sangare', email_address: 'issa@issa.com', password: 'issa'})
+mikec = User.create({first_name: 'Michael', last_name: 'Causey', email_address: 'mikec@mikec.com', password: 'mikec'})
+steve = User.create({first_name: 'Steve', last_name: 'Pesce', email_address: 'steve@steve.com', password: 'steve'})
+jr = User.create({first_name: 'Johnnathan', last_name: 'Baxter', email_address: 'jr@jr.com', password: 'jr'})
+tien = User.create({first_name: 'Tien', last_name: 'Phan', email_address: 'tien@tien.com', password: 'tien'})
+shawn = User.create({first_name: 'Shawn', last_name: 'Alexander', email_address: 'shawn@shawn.com', password: 'shawn'})
+luis = User.create({first_name: 'Luis', last_name: 'Fernandez', email_address: 'luis@luis.com', password: 'luis'})
+marcos = User.create({first_name: 'Marcos', last_name: 'Velasco', email_address: 'marcos@marcos.com', password: 'marcos'})
+mikej = User.create({first_name: 'Mike', last_name: 'Jester', email_address: 'mikej@mikej.com', password: 'mikej'})
+danny = User.create({first_name: 'Danny', last_name: 'Coop', email_address: 'danny@danny.com', password: 'danny'})
+
+
+
 
 def day
     rand(27) + 1
@@ -39,12 +58,12 @@ def year
 end
 
 30.times do 
-    kristin.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: kristin.id, latitude: random_deg(38.8119, 38.9955), longitude: random_deg(-76.9093, -77.1515), zoom: 12.80, date: DateTime.new(year,rand(1..12),day,rand(5..20),minute)})
-    andy.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: andy.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(year,rand(1..12),day,rand(5..20),minute)})
-    dad.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: dad.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(year,rand(1..12),day,rand(5..20),minute)})
-    val.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: val.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(year,rand(1..12),day,rand(5..20),minute)})
-    ali.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: ali.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(year,rand(1..12),day,rand(5..20),minute)})
-    mom.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: mom.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(year,rand(1..12),day,rand(5..20),minute)})
+    kristin.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: kristin.id, latitude: random_deg(38.8119, 38.9955), longitude: random_deg(-76.9093, -77.1515), zoom: 12.80, date: DateTime.new(2020,rand(6..12),day,rand(5..20),minute)})
+    andy.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: andy.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(2020,rand(6..12),day,rand(5..20),minute)})
+    dad.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: dad.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(2020,rand(6..12),day,rand(5..20),minute)})
+    val.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: val.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(2020,rand(6..12),day,rand(5..20),minute)})
+    ali.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: ali.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(2020,rand(6..12),day,rand(5..20),minute)})
+    mom.runs << Run.create({name: Faker::Quote.most_interesting_man_in_the_world, distance: rand(11), user_owner_id: mom.id, longitude: random_deg(-76.9093, -77.1515), latitude: random_deg(38.8119, 38.9955), zoom: 12.80, date: DateTime.new(2020,rand(6..12),day,rand(5..20),minute)})
 end
 
 puts "HOLY SHIT IT WORKED!"
